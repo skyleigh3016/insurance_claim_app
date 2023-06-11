fd0512159cf27d0d110e103d35eff600ab6f2c8c
skyleigh3016 <babyskyleigh16@gmail.com>
2023-06-11 09:35:19 +0800

    update 6/11/2023-up

--- src/main/java/com/OJTProject/InsuranceAndClaimApp/repository/PdfFileRepository.java
+++ src/main/java/com/OJTProject/InsuranceAndClaimApp/repository/PdfFileRepository.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PdfFileRepository extends JpaRepository<PdfFile, Long> {
-    @Query(value = "select sum(c.claim_amount ) from pdf_claims c",nativeQuery = true)
-    int totalClaimed ();
-    @Query(value = "select sum(c.claim_amount ) from pdf_claims c  WHERE c.user.email LIKE CONCAT('%', :email, '%')")
-    int totalMyClaimed (String email);
+//    @Query(value = "select sum(c.claim_amount ) from pdf_claims c",nativeQuery = true)
+//    int totalClaimed ();
+//    @Query(value = "select sum(c.claim_amount ) from pdf_claims c  WHERE c.user.email LIKE CONCAT('%', :email, '%')")
+//    int totalMyClaimed (String email);
     List<PdfFile> findByUserId(long id);
 }
